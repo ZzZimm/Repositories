4c5b7d7125c56e3149572987148eac8a09e9e438
Lucas ZIMMERMANN <lzimmerm@e2r9p10.42.fr>
2019-04-01 19:47:39 +0200

    misunderstanding on lstdel

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: lzimmerm <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/01 14:04:27 by lzimmerm          #+#    #+#             */
-/*   Updated: 2019/04/01 15:14:32 by lzimmerm         ###   ########.fr       */
+/*   Updated: 2019/04/01 19:45:21 by lzimmerm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	ft_lstdel(t_list **alst, void (*del)(void*, size_t))
 {
+	t_list *tmp;
+
 	while (*alst)
 	{
+		tmp = (*alst)->next;
 		del((*alst)->content, (*alst)->content_size);
 		(*alst)->content_size = 0;
 		free(*alst);
-		*alst = (*alst)->next;
+		*alst = tmp;
 	}
 	*alst = NULL;
 }
